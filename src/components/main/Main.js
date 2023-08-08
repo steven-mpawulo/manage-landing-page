38a78322472fe004bf6d8f138933395be562cdac
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-08 11:30:42 +0300

    specifies height and width of button

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Illustration from '../../images/illustration-intro.svg'
 import './main.css'
+import Button from '../common/button/Button'
 
 const Main = () => {
   return (
@@ -10,6 +11,8 @@ const Main = () => {
         <div className="first-section-left">
           <h1>Bring everyone together to build better products</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ducimus sit, maxime alias et veritatis praesentium eos! Voluptatem a perspiciatis ipsa sapiente facilis doloribus, modi placeat dignissimos distinctio error voluptatibus.</p>
+          
+          <Button buttonText={"Get Started"} className="button-inner"/>
         </div>
         <div className="first-section-right">
           <img src={Illustration} alt="illustration-intro" className='illustration' />
