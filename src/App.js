c4b00387a44cc0c6dda36de412643ddab10348cb
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-08 13:16:50 +0300

    adds background image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
+import './App.css'
 
 function App() {
   return (
