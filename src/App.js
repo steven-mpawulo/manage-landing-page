4a1f0162e3c68cfd469c733687bf56363309e7bb
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-08 10:21:34 +0300

    connects header main and footer js components to app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../header/Header'
+
+const Main = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Main
