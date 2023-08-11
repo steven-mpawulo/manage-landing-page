e118b69fb088fa70e8b8629a72eb345ab029bab4
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-11 12:14:39 +0300

    sets up initial footer html and css

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,6 +8,12 @@ const Footer = () => {
           <p>Simplify how your team <span>works today</span></p>
           <button className='fourth-container-button'>Get Started</button>
       </div>
+      <footer>
+        <div className="footer-first-container"></div>
+        <div className="footer-second-container"></div>
+        <div className="footer-third-container"></div>
+        <div className="footer-fourth-container"></div>
+      </footer>
     </div>
   )
 }
