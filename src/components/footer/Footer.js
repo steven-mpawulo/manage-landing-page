43fd15941f6a629f9c9468e1bdd868fc66a0ce2a
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-11 12:07:17 +0300

    styles fourth section container

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,12 @@
 import React from 'react'
+import "./footer.css"
 
 const Footer = () => {
   return (
     <div className='footer'>
       <div className="fourth-section">
-        <div className="fourth-secion-left-container">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia quidem reiciendis nesciunt unde qui vitae quisquam dolorum laudantium, nemo, quis et quam minus id nam soluta obcaecati veritatis animi nobis?</p>
-        </div>
-        <div className="fourth-section-right-container">
+          <p>Simplify how your team <span>works today</span></p>
           <button className='fourth-container-button'>Get Started</button>
-        </div>
       </div>
     </div>
   )
